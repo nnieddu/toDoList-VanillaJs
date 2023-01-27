3c46408beb5f1962d76bfe079fe10d9a94f85cbe
nnieddu <nicolas.nieddu@free.fr>
2023-01-27 11:27:14 +0100

    finish and fix editTask feature

--- scripts/editTask.js
+++ scripts/editTask.js
@@ -1,13 +1,13 @@
-
-// <!-- La possibilité d'editer une tâche -->
+// <! ------------- La possibilité d'editer une tâche ------------- >
 
 export function editListItem(listItem) {
   const input = document.createElement("input");
   input.value = listItem.getElementsByTagName("span")[0].innerText;
 
-	const inputDate = document.createElement("input");
-  inputDate.setAttribute("type", "date");
-	inputDate.valueAsDate = new Date(listItem.getAttribute("data-end-date"));
+  const inputDueDate = document.createElement("input");
+  inputDueDate.setAttribute("type", "date");
+  inputDueDate.valueAsDate = new Date(listItem.getAttribute("data-end-date"));
+  inputDueDate.className += " task-end-date";
 
   const saveButton = document.createElement("img");
   saveButton.innerText = "Save";
@@ -16,16 +16,34 @@ export function editListItem(listItem) {
 
   listItem.innerText = "";
   listItem.appendChild(input);
-  listItem.appendChild(inputDate);
+  listItem.appendChild(inputDueDate);
   listItem.appendChild(saveButton);
 
   saveButton.addEventListener("click", () => {
-    fetch(`http://127.0.0.1:9000/v1/tasks/${listItem.getAttribute("data-label")}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedTask),
-    }).catch((error) => console.error(error));
+    if (input.value && inputDueDate.value) {
+      const updatedTask = {
+        label: listItem.getAttribute("data-label"),
+        description: input.value,
+        start_date: new Date(listItem.getAttribute("data-start-date")).toISOString(),
+        end_date: new Date(inputDueDate.value).toISOString(),
+      };
+
+      fetch(`http://127.0.0.1:9000/v1/tasks/${listItem.getAttribute("data-label")}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedTask),
+      }).catch((error) => console.error(error));
+
+			listItem.getElementsByTagName("span")[0].innerText = input.value;
+			listItem.getElementsByTagName("span")[1].innerText = inputDueDate.value;
+
+      listItem.removeChild(input);
+      listItem.removeChild(inputDueDate);
+      listItem.removeChild(saveButton);
+    }
+		else
+			alert("Task and due date can't be empty !")
   });
 }
